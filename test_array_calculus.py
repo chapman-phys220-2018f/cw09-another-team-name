26030ac8a07c88230e369ee5b9255492745cf515
jacob-anabi <anabi@chapman.edu>
2018-11-08 01:13:29 +0000

    added test module

---
+++ test_array_calculus.py
@@ -0,0 +1,21 @@
+"""
+test_array_calculus.py Test Module Description:
+
+Checks if the derivative function in array_calculus.py is correct
+
+Functions:
+test_derivative - tests the derivative function of array_calculus.py
+"""
+
+import math
+import array_calculus as ac
+
+def test_derivative():
+    """
+    Tests whether the central finite difference
+    is close to the derivative of the function
+    """
+    f = lambda x: x**2
+    Df = ac.derivative(f)
+    for i in range (1, len(Df)-1):
+        assert math.isclose(Df[i], 2*i)
